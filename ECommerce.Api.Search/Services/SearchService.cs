2871c94982184471c4963eb3104072dd87c24cc0
amielc <amielc1@gmail.com>
2021-02-01 07:25:56 +0200

    Add ProductService To SearchService

--- ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -9,16 +9,28 @@ namespace ECommerce.Api.Search.Services
     public class SearchService : ISearchService
     {
         private readonly IOrdersService ordersService;
+        private readonly IProductService productService;
 
-        public SearchService(IOrdersService ordersService)
+        public SearchService(IOrdersService ordersService, IProductService productService)
         {
             this.ordersService = ordersService;
+            this.productService = productService;
         }
         public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
         {
             var orderResult = await ordersService.GetOrdersAsnc(customerId);
+            var productsResult = await productService.GetProductsAsync();
             if (orderResult.IsSuccess)
             {
+                foreach (var order in orderResult.Orders)
+                {
+                    foreach (var item in order.Items)
+                    {
+                        item.ProductName = productsResult.IsSuccess ?
+                            productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId).Name:
+                            "Product information is not available";
+                    }
+                }
                 var result = new
                 {
                     Orders = orderResult.Orders
