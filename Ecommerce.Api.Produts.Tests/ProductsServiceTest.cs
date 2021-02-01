44fddab308b2b431e884c4e4f8796c86ecee8051
Amiel Cohen <amielc1@gmail.com>
2021-02-01 21:36:26 +0200

    Add Unit Testing

---
+++ Ecommerce.Api.Produts.Tests/ProductsServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ecommerce.Api.Produts.Tests
+{
+    public class ProductsServiceTest
+    {
+        [Fact]
+        public void GetProductsReturnAllProducts()
+        {
+
+        }
+    }
+}
