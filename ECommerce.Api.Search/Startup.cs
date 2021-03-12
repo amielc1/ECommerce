aef78015fba2f4ee9e552bef255f488ddd720264
Amiel Cohen <amielc1@gmail.com>
2021-03-12 13:53:58 +0200

    remove unusing namespaces

--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -2,16 +2,11 @@ using ECommerce.Api.Search.Interfaces;
 using ECommerce.Api.Search.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Polly;
+using System;
 
 namespace ECommerce.Api.Search
 {
